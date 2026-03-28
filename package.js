64b8c4c6f845510bff6a6097d6bc9f25a35067da
DVZdev <141517438+DVZdev@users.noreply.github.com>
2026-03-28 12:05:01 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "backend-expedicao-postgres",
+  "version": "1.0.0",
+  "description": "Servidor para sistema de estoque com PostgreSQL",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "pg": "^8.11.0",
+    "cors": "^2.8.5"
+  }
+}
